6db5d6fd0c06caa335b10255a6fee2f20b2847b3
Angel Pineda <pinedabob77@gmail.com>
2023-07-06 17:30:51 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 
-// Fetch  data from db.json
-fetch('http://localhost:3000/images')
-  .then(response => response.json())
-  .then(data => {
-    // Get the first 5 image URLs from the data array
-    data.forEach(card => {
-// create elements 
+function createCard(card){
+
+
+
       const cardElement = document.createElement('div');
       cardElement.classList.add('card');
-//create image element 
+
       const imageElement = document.createElement('img');
       imageElement.src = card.imageUrl;
-//create card name
+
       const nameElement = document.createElement('h2');
       nameElement.textContent = card.name;
-//create meme description 
+
       const descriptionElement = document.createElement('p');
       descriptionElement.textContent = card.description;
-      // create upvote button and the ability to keep count of upvotes
+      
       const upvoteButton = document.createElement('button');
       upvoteButton.textContent = 'Upvote';
       const upvoteCount = document.createElement('span');
@@ -28,7 +25,7 @@ fetch('http://localhost:3000/images')
         
       
       });
-// create downvote button and the ability to create count of down votes
+
       const downvoteButton = document.createElement('button');
       downvoteButton.textContent = 'Downvote';
       const downvoteCount = document.createElement('span');
@@ -39,42 +36,40 @@ fetch('http://localhost:3000/images')
         
       });
 
-      cardElement.appendChild(imageElement);
-      cardElement.appendChild(nameElement);
-      cardElement.appendChild(descriptionElement);
-      cardElement.appendChild(upvoteButton);
-      cardElement.appendChild(upvoteCount);
-      cardElement.appendChild(downvoteButton);
-      cardElement.appendChild(downvoteCount);
+      cardElement.append(imageElement,nameElement, descriptionElement, upvoteButton, upvoteCount, downvoteButton, downvoteCount);
 
       document.body.appendChild(cardElement);
 
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
+    return cardElement;
+}
+   
+fetch('http://localhost:3000/images')
+.then(response => response.json())
+.then(data => {
+  data.forEach(card => {
+    const cardElement = createCard(card);
+    document.body.appendChild(cardElement);
+
   });
- 
-  
- 
+})
 
 function addComment() {
-  // USER INPUT 
+
   const commentInput = document.getElementById("commentInput");
   const comment = commentInput.value;
-// incase user tries to submit comment with blank statement
+
   if (comment.trim() === '') {
     alert("Please add comment before submitting !");
     return;
   }
-  // create new comment element
+
   const commentList = document.getElementById("commentList");
   const commentItem = document.createElement("li");
   const commentText = document.createTextNode(comment);
   commentItem.appendChild(commentText);
   commentList.appendChild(commentItem);
 
-  // clear user input
+
   commentInput.value = "";
 }
 
@@ -85,9 +80,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   button.addEventListener('click', function() {
     if (body.style.backgroundColor === 'rgb(255, 255, 255)') {
-      body.style.backgroundColor = '#000'; // Change to black
+      body.style.backgroundColor = '#000'; 
     } else {
-      body.style.backgroundColor = '#fff'; // Change to white
+      body.style.backgroundColor = '#fff'; 
     }
   });
 });
